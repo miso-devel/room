0d7c46ffb28c7b7ed19da67f211f2d91a970d672
miso-devel <kengo279@icloud.com>
2023-10-28 03:16:46 +0900

    既存APIのSecret変更

--- server/controller/members/index.ts
+++ server/controller/members/index.ts
@@ -1,14 +1,14 @@
 import { Bot } from "../../bot/main.ts";
-import { Secret } from "../../secret.ts";
 import { toMember } from "./util.ts";
 import { components } from "../../types/schema.ts";
 import { Env, Handler } from "https://deno.land/x/hono@v3.7.2/types.ts";
+import { SECRET } from "../../secret.ts";
 
 type TMember = components["schemas"]["User"];
 
 // TODO: Handler<Env, "/members", {}, Promise<Response>>とかの型は結構無理やりしてる感じがあるので修正できるならする
 const index: Handler<Env, "/members", {}, Promise<Response>> = async (c) => {
-  const data = await Bot.helpers.getMembers(Secret.GUILD_ID, { limit: 300 });
+  const data = await Bot.helpers.getMembers(SECRET.GUILD_ID, { limit: 300 });
   const members: TMember[] = [];
   data.forEach((value, _) => members.push(toMember(value)));
   return c.json(members);
@@ -16,7 +16,7 @@ const index: Handler<Env, "/members", {}, Promise<Response>> = async (c) => {
 
 const show: Handler<Env, "/members/:id", {}, Promise<Response>> = async (c) => {
   const id = c.req.param("id");
-  const data = await Bot.helpers.getMember(Secret.GUILD_ID, BigInt(id));
+  const data = await Bot.helpers.getMember(SECRET.GUILD_ID, BigInt(id));
   if (!id) return c.json({ error: "メンバーが存在しませんでした" });
   return c.json(toMember(data));
 };
