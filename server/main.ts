fa078c892ccc22cdfd10307f194a4f50bb5d9951
miso-devel <kengo279@icloud.com>
2023-10-28 03:17:15 +0900

    雑にcors設定

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,29 @@
 import { Member } from "./controller/members/index.ts";
 import { Workshop } from "./controller/workshop/index.ts";
-import { Hono } from "./mod.ts";
+import { cors, Hono, logger, poweredBy } from "./mod.ts";
+import { SECRET } from "./secret.ts";
 
 const app = new Hono();
+
+app.use(
+  "*",
+  cors({
+    origin: [SECRET.CLIENT_URL as string],
+    allowHeaders: [
+      "Origin",
+      "Content-Type",
+      "Authorization",
+      "X-Custom-Header",
+      "Access-Control-Allow-Origin",
+    ],
+    allowMethods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+    exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
+    maxAge: 600,
+  }),
+  logger(),
+  poweredBy(),
+);
 
 // members API
 app.get("/members", Member.index);
