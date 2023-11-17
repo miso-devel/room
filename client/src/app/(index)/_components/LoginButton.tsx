1b1e84b3b43141aee61eb6ea034633c45fa4bc24
miso-devel <kengo279@icloud.com>
2023-11-17 23:53:29 +0900

    ログイン時のリダイレクト先を設定

--- client/src/app/(index)/_components/LoginButton.tsx
+++ client/src/app/(index)/_components/LoginButton.tsx
@@ -3,7 +3,10 @@ import { signIn } from 'next-auth/react';
 
 export const LoginButton = async () => {
   return (
-    <button onClick={() => signIn()} className="btn btn-outline btn-secondary">
+    <button
+      onClick={() => signIn('discord', { callbackUrl: `${process.env.NEXT_PUBLIC_CLIENT_URL}/home` })}
+      className="btn btn-outline btn-secondary"
+    >
       Login with Discord
     </button>
   );
