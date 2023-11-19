46f8c9f3326252e30caaae0e0823ebf5c87ff09b
miso-devel <kengo279@icloud.com>
2023-11-19 13:35:19 +0900

    schemaの更新をする

--- client/src/types/schema.ts
+++ client/src/types/schema.ts
@@ -3,109 +3,78 @@
  * Do not make direct changes to the file.
  */
 
+
 export interface paths {
-    "/members": {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
+  "/members": {
+    /** Members */
+    get: {
+      responses: {
+        /** @description Success */
+        200: {
+          content: {
+            "application/json": components["schemas"]["User"];
+          };
         };
-        /** Members */
-        get: {
-            parameters: {
-                query?: never;
-                header?: never;
-                path?: never;
-                cookie?: never;
-            };
-            requestBody?: never;
-            responses: {
-                /** @description Success */
-                200: {
-                    headers: {
-                        [name: string]: unknown;
-                    };
-                    content: {
-                        "application/json": components["schemas"]["User"];
-                    };
-                };
-            };
-        };
-        put?: never;
-        post?: never;
-        delete?: never;
-        options?: never;
-        head?: never;
-        patch?: never;
-        trace?: never;
+      };
     };
-    "/members/{userId}": {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
+  };
+  "/members/{userId}": {
+    /** Member */
+    get: {
+      parameters: {
+        path: {
+          userId: number;
         };
-        /** Member */
-        get: {
-            parameters: {
-                query?: never;
-                header?: never;
-                path: {
-                    userId: number;
-                };
-                cookie?: never;
-            };
-            requestBody?: never;
-            responses: {
-                /** @description Success */
-                200: {
-                    headers: {
-                        [name: string]: unknown;
-                    };
-                    content: {
-                        "application/json": components["schemas"]["User"];
-                    };
-                };
-            };
+      };
+      responses: {
+        /** @description Success */
+        200: {
+          content: {
+            "application/json": components["schemas"]["User"];
+          };
         };
-        put?: never;
-        post?: never;
-        delete?: never;
-        options?: never;
-        head?: never;
-        patch?: never;
-        trace?: never;
+      };
     };
+  };
 }
+
 export type webhooks = Record<string, never>;
+
 export interface components {
-    schemas: {
-        User: {
-            id: string;
-            name: string;
-            avatar: string;
-            joinedAt?: number;
-        };
-        Workshop: {
-            id: string;
-            title: string;
-            description?: string;
-        };
-        Event: {
-            id: string;
-            workshopId?: string;
-            theme: string;
-            date: string;
-            isCronTarget: boolean;
-        };
+  schemas: {
+    User: {
+      id: string;
+      name: string;
+      avatar: string;
+      joinedAt?: number;
     };
-    responses: never;
-    parameters: never;
-    requestBodies: never;
-    headers: never;
-    pathItems: never;
+    Workshop: {
+      id: string;
+      title: string;
+      description?: string;
+    };
+    Event: {
+      id: string;
+      workshopId?: string;
+      theme: string;
+      date: string;
+      isCronTarget: boolean;
+    };
+    Participant: {
+      id: string;
+      eventId: string;
+      memberId: string;
+    };
+  };
+  responses: never;
+  parameters: never;
+  requestBodies: never;
+  headers: never;
+  pathItems: never;
 }
+
 export type $defs = Record<string, never>;
+
+export type external = Record<string, never>;
+
 export type operations = Record<string, never>;
