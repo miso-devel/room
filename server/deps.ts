448d70d192a1ef70a2b4348222b24920aea35f26
miso-devel <kengo279@icloud.com>
2023-12-02 02:04:12 +0900

    deps更新

--- server/deps.ts
+++ server/deps.ts
@@ -1,21 +1,28 @@
+// discordeno
 export * from "https://deno.land/x/discordeno@18.0.1/mod.ts";
+export {
+  Collection,
+  type Member,
+} from "https://deno.land/x/discordeno@18.0.1/mod.ts";
+
+// Hono
 export * as dotenv from "https://deno.land/std@0.190.0/dotenv/load.ts";
-export type { Response } from "https://deno.land/x/hono@v3.7.2/client/types.ts";
+export type { Response } from "https://deno.land/x/hono@v3.10.3/client/types.ts";
 export {
   type Context,
   type Env,
   type Handler,
   Hono,
-} from "https://deno.land/x/hono@v3.7.2/mod.ts";
-export type { TypedResponse } from "https://deno.land/x/hono@v3.7.2/mod.ts";
+  HTTPException,
+} from "https://deno.land/x/hono@v3.10.3/mod.ts";
 export {
   cors,
-  logger,
-  poweredBy,
-} from "https://deno.land/x/hono@v3.7.2/middleware.ts";
-export { load } from "https://deno.land/std@0.194.0/dotenv/mod.ts";
-export {
   deleteCookie,
   getCookie,
+  logger,
+  poweredBy,
   setCookie,
-} from "https://deno.land/x/hono@v3.3.0/middleware.ts";
+} from "https://deno.land/x/hono@v3.10.3/middleware.ts";
+
+// dotenv
+export { load } from "https://deno.land/std@0.194.0/dotenv/mod.ts";
