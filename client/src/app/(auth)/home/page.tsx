bb695d61b67d23ca18e927ec3009635a125a5d97
miso-devel <kengo279@icloud.com>
2023-12-08 16:26:33 +0900

    home修正

--- client/src/app/(auth)/home/page.tsx
+++ client/src/app/(auth)/home/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+import { schema } from '@/types/common';
+import { Workshops } from './_components/workshops';
+
+export default async function Home() {
+  const workshops: schema['Workshop'][] = await fetch(`${process.env.SERVER_URL}/workshops`, {
+    cache: 'no-cache',
+  }).then(async (data) => await data.json());
+
   return (
-    <div className="prose ">
-      <h1>Home</h1>
+    <div className="grid grid-cols-2 gap-5">
+      <Workshops workshops={workshops} />
     </div>
   );
 }
