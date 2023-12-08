5ff8ba3cb57ce8066c488bab8e0d03d148c7d60c
miso-devel <kengo279@icloud.com>
2023-12-08 16:26:54 +0900

    daisyuiを剥がす

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -7,11 +7,36 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
-  daisyui: {
-    themes: ['night'],
+    extend: {
+      backgroundColor: {
+        primary: '#293CA6',
+        secondary: '#3854A6',
+        accent: '#F2E205',
+        bright: '#F2F2F2',
+        dark: '#0D0D0D',
+      },
+      textColor: {
+        primary: '#293CA6',
+        secondary: '#3854A6',
+        accent: '#F2E205',
+        bright: '#F2F2F2',
+        dark: '#0D0D0D',
+      },
+      borderColor: {
+        primary: '#293CA6',
+        secondary: '#3854A6',
+        accent: '#F2E205',
+        bright: '#F2F2F2',
+        dark: '#0D0D0D',
+      },
+      fill: {
+        bright: '#F2F2F2',
+        dark: '#0D0D0D',
+      },
+      accentColor: {
+        accent: '#F2E205',
+      },
+    },
   },
 };
 export default config;
