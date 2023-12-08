7837da7bfd092eff1e6ed49861a1a717ae9478be
miso-devel <kengo279@icloud.com>
2023-12-08 16:27:50 +0900

    memberページ修正

--- client/src/app/(auth)/members/page.tsx
+++ client/src/app/(auth)/members/page.tsx
@@ -1,7 +1,8 @@
 export default function Members() {
   return (
-    <div className="prose">
-      <h1>Members</h1>
-    </div>
+    <>
+      <h1 className="text-3xl">メンバー一覧</h1>
+      <p className="my-5">一旦メンバー一覧とかは取れなくてもいい気がするので実装はしていない</p>
+    </>
   );
 }
