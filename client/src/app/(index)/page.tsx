eac608788de2cf6ae375744b91c5e7c426198a48
miso-devel <kengo279@icloud.com>
2023-12-08 16:29:35 +0900

    非ログイン時のページ修正

--- client/src/app/(index)/page.tsx
+++ client/src/app/(index)/page.tsx
@@ -2,9 +2,9 @@ import { LoginButton } from './_components/LoginButton';
 
 export default function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-primary">
       <div className="prose text-center">
-        <h1>Doer Member Site</h1>
+        <h1 className=" text-3xl">Doer Member Site</h1>
         <LoginButton />
       </div>
     </main>
