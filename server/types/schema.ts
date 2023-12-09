4b0dc1e44e8163d511b4ba6e662a59f63f8c6655
miso-devel <kengo279@icloud.com>
2023-12-09 17:49:35 +0900

    スキーマ更新

--- server/types/schema.ts
+++ server/types/schema.ts
@@ -10,13 +10,57 @@ export type webhooks = Record<string, never>;
 
 export type components = {
   schemas: {
-    User: external["schemas/user/model.json"];
-    Workshop: external["schemas/workshop/model.json"];
-    WorkshopInput: external["schemas/workshop/input.json"];
-    Event: external["schemas/event/model.json"];
-    EventInput: external["schemas/event/input.json"];
-    EventOutput: external["schemas/event/output.json"];
-    Speaker: external["schemas/speaker/model.json"];
+    User: {
+      id: string;
+      name: string;
+      avatar: string;
+      joinedAt: number;
+    };
+    Workshop: {
+      id: string;
+      title: string;
+      description?: string;
+      createdAt: number;
+      updatedAt: number;
+    };
+    WorkshopInput: {
+      title: string;
+      description?: string;
+    };
+    Event: {
+      id: string;
+      workshopId: string;
+      theme: string;
+      date: string;
+      isCronTarget: boolean;
+      createdAt: number;
+      updatedAt: number;
+    };
+    EventInput: {
+      event: {
+        workshopId: string;
+        theme: string;
+        date: string;
+      };
+      speakerIds: string[];
+    };
+    EventOutput: {
+      speakers: unknown[];
+      id: string;
+      workshopId: string;
+      theme: string;
+      date: string;
+      isCronTarget: boolean;
+      createdAt: number;
+      updatedAt: number;
+    };
+    Speaker: {
+      id: string;
+      eventId: string;
+      memberId: string;
+      createdAt: number;
+      updatedAt: number;
+    };
   };
   responses: never;
   parameters: never;
@@ -27,52 +71,6 @@ export type components = {
 
 export type $defs = Record<string, never>;
 
-export type external = {
-  "schemas/event/input.json": {
-    event: {
-      workshopId: external["schemas/event/model.json"]["workshopId"];
-      theme: external["schemas/event/model.json"]["theme"];
-      date: external["schemas/event/model.json"]["date"];
-    };
-    speakerIds: string[];
-  };
-  "schemas/event/model.json": {
-    id: string;
-    workshopId: string;
-    theme: string;
-    date: string;
-    isCronTarget: boolean;
-    createdAt: number;
-    updatedAt: number;
-  };
-  "schemas/event/output.json": {
-    event: external["schemas/event/model.json"];
-    speakers: external["schemas/speaker/model.json"][];
-  };
-  "schemas/speaker/model.json": {
-    id: string;
-    eventId: string;
-    memberId: string;
-    createdAt: number;
-    updatedAt: number;
-  };
-  "schemas/user/model.json": {
-    id: string;
-    name: string;
-    avatar: string;
-    joinedAt: number;
-  };
-  "schemas/workshop/input.json": {
-    title: string;
-    description?: string;
-  };
-  "schemas/workshop/model.json": {
-    id: string;
-    title: string;
-    description?: string;
-    createdAt: number;
-    updatedAt: number;
-  };
-};
+export type external = Record<string, never>;
 
 export type operations = Record<string, never>;
