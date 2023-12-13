7e2870bff1f0b7b66e22815d97b3f2cf3b108ced
miso-devel <kengo279@icloud.com>
2023-12-14 05:08:15 +0900

    svgの設定

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -13,7 +13,9 @@ const config: Config = {
         secondary: '#293CA6',
         accent: '#F2E205',
         bright: '#D9D9D9',
-        middle: '#6F6F73',
+        middleBright: '#e8e8e8',
+        middle: '#D9D9D9',
+        middleDark: '#6F6F73',
         dark: '#0D0D0D',
       },
       textColor: {
