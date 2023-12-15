0f5aaac136b93e3d914079f087ef1173df918153
miso-devel <kengo279@icloud.com>
2023-12-16 05:00:48 +0900

    applayout周りスッキリさせる

--- client/components/layout/AppLayout/PageContent.tsx
+++
@@ -1,7 +0,0 @@
-import { FC, ReactNode } from 'react';
-
-type TPageContent = { children: ReactNode };
-
-export const PageContent: FC<TPageContent> = ({ children }) => {
-  return <div className="w-[85%] m-10 text-dark">{children}</div>;
-};
