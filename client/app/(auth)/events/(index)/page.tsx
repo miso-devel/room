67b16d529b5415377131481e5d4f47754c50b6c2
miso-devel <kengo279@icloud.com>
2023-12-17 23:45:44 +0900

    event一覧を雑に実装

--- client/app/(auth)/events/(index)/page.tsx
+++ client/app/(auth)/events/(index)/page.tsx
@@ -1,18 +1,25 @@
-import { components } from '../../../../types/schema';
+import Link from 'next/link';
+import { schema } from '../../../../types/common';
+import { fetcher } from '../../../../util/fetcher';
 
 export default async function EventsPage() {
-  const events: components['schemas']['Event'][] = await fetch(`${process.env.SERVER_URL}/events`).then(async (data) =>
-    data.json()
-  );
-
+  const events = await fetcher.get<schema['Event'][]>('/events', { cache: 'no-cache' });
   return (
-    <div className="prose">
-      <div className="flex gap-3">
-        <h1>events</h1>
-        <a className="btn btn-outline btn-secondary" role="button" href="/events/new">
-          作成
-        </a>
+    <>
+      <div className="grid grid-cols-1 gap-3">
+        {events.map((event) => {
+          return (
+            <Link
+              key={event.id}
+              href={`/workshops/${event.workshopId}`}
+              className="border-b-2 py-2 transition-all hover:border-b-primary"
+            >
+              <h2 className="mb-2 text-xl">{event.theme}</h2>
+              {/* <p>event</p> */}
+            </Link>
+          );
+        })}
       </div>
-    </div>
+    </>
   );
 }
--- client/constants/paths.ts
+++ client/constants/paths.ts
@@ -2,4 +2,5 @@ export const PATHS = [
   { name: '/home', text: 'HOME' },
   { name: '/members', text: 'MEMBER' },
   { name: '/workshops', text: 'WORKSHOP' },
+  { name: '/events', text: 'EVENT' },
 ];
