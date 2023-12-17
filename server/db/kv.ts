8fe64ae57d02d1db76d678ea3fe00fb2ebac958b
miso-devel <kengo279@icloud.com>
2023-12-17 23:52:35 +0900

    kv操作用の汎用関数調整

--- server/db/kv.ts
+++ server/db/kv.ts
@@ -2,50 +2,42 @@
 
 import { PREFIX } from "../constants/prefix.ts";
 import { SECRET } from "../constants/secret.ts";
+import { throwAPIError } from "../util/throwError.ts";
 
 const kv = await Deno.openKv(SECRET.ENV === "dev" ? "dev.db" : "prod.db");
-type id = { id: string };
-type prefix = { prefix: PREFIX };
-type data<T> = { data: T };
 type basic = { id: string; createdAt: number; updatedAt: number };
-type KvRes<T> = Deno.KvEntryMaybe<T>;
 
-type TfetchAll = prefix;
-type TFetchOne = id & prefix;
-type TCreate<T extends basic> = prefix & data<T>;
-type TUpdate<T extends basic> = prefix & data<T>;
-type TDelete = id & prefix;
-
-const fetchAll = async <T>({ prefix }: TfetchAll): Promise<T[]> => {
+const fetchAll = async <T>(prefix: PREFIX): Promise<T[]> => {
   const datas: Deno.KvListIterator<T> = kv.list({ prefix: [prefix] });
   const result: T[] = [];
   for await (const { value } of datas) result.push(value);
   return result;
 };
 
-const fetchOne = async <T>({ id, prefix }: TFetchOne): Promise<KvRes<T>> => {
-  return await kv.get([prefix, id]) as KvRes<T>;
+const fetchOne = async <T>(prefix: PREFIX, id: string): Promise<T> => {
+  return (await kv.get([prefix, id])).value as T;
 };
 
-const createOne = async <
-  T extends basic,
->(
-  { data, prefix }: TCreate<T>,
-): Promise<KvRes<T>> => {
+const createOne = async <T extends basic>(
+  prefix: PREFIX,
+  data: T,
+): Promise<T> => {
+  if (data.id === undefined) throwAPIError(500, "data id is undefined!!!!!!")();
   await kv.set([prefix, data.id], { ...data });
-  return await kv.get([prefix, data.id]) as KvRes<T>;
+  return (await kv.get([prefix, data.id])).value as T;
 };
 
 const updateOne = async <
   T extends basic,
 >(
-  { prefix, data }: TUpdate<T>,
-): Promise<KvRes<T>> => {
+  prefix: PREFIX,
+  data: T,
+): Promise<T> => {
   await kv.set([prefix, data.id], data);
-  return await kv.get([prefix, data.id]) as KvRes<T>;
+  return (await kv.get([prefix, data.id])).value as T;
 };
 
-const deleteOne = async ({ id, prefix }: TDelete) => {
+const deleteOne = async (prefix: PREFIX, id: string) => {
   await kv.delete([prefix, id]);
 };
 
