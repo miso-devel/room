ab0ce44362a5f1efbff9f03380a85632b64d8458
miso-devel <kengo279@icloud.com>
2023-12-19 00:38:14 +0900

    fontをtailwindで変更するように

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body className="font-mono">{children}</body>
     </html>
   );
 }
