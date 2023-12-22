4fa8f6a7763f627f25552fb25cf8a8c0d3a2b7ee
miso-devel <kengo279@icloud.com>
2023-12-22 23:53:12 +0900

    fetcherにpatch,deleteを追加

--- client/util/fetcher/index.ts
+++ client/util/fetcher/index.ts
@@ -4,7 +4,7 @@ const get = async <T>(url: string, init?: RequestInit): Promise<T> => {
   return res;
 };
 
-const post = async <I, T>(url: string, data: I): Promise<T> => {
+const post = async <I, T>(url: string, data: I, init?: RequestInit): Promise<T> => {
   const baseUrl = typeof window ? process.env.NEXT_PUBLIC_SERVER_URL : process.env.SERVER_URL;
   const res = await fetch(baseUrl + url, {
     method: 'POST',
@@ -12,6 +12,33 @@ const post = async <I, T>(url: string, data: I): Promise<T> => {
     body: JSON.stringify(data),
     mode: 'cors',
     credentials: 'include',
+    ...init,
+  }).then((res) => res.json());
+  return res;
+};
+
+const patch = async <I, T>(url: string, data: I, init?: RequestInit): Promise<T> => {
+  const baseUrl = typeof window ? process.env.NEXT_PUBLIC_SERVER_URL : process.env.SERVER_URL;
+  const res = await fetch(baseUrl + url, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+    mode: 'cors',
+    credentials: 'include',
+    ...init,
+  }).then((res) => res.json());
+  return res;
+};
+
+const _delete = async <I, T>(url: string, data: I, init?: RequestInit): Promise<T> => {
+  const baseUrl = typeof window ? process.env.NEXT_PUBLIC_SERVER_URL : process.env.SERVER_URL;
+  const res = await fetch(baseUrl + url, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+    mode: 'cors',
+    credentials: 'include',
+    ...init,
   }).then((res) => res.json());
   return res;
 };
@@ -19,4 +46,6 @@ const post = async <I, T>(url: string, data: I): Promise<T> => {
 export const fetcher = {
   get,
   post,
+  patch,
+  _delete,
 };
