1d149de7c6911debd44c754d355bcc32d4127743
miso-devel <kengo279@icloud.com>
2024-01-01 22:44:41 +0900

    authに必要な環境変数を準備する

--- server/constants/secret.ts
+++ server/constants/secret.ts
@@ -2,8 +2,13 @@ import "https://deno.land/std@0.190.0/dotenv/load.ts";
 
 export const SECRET = {
   CLIENT_URL: Deno.env.get("CLIENT_URL"),
+  SERVER_URL: Deno.env.get("SERVER_URL"),
   DISCORD_TOKEN: Deno.env.get("DISCORD_TOKEN")!,
   GUILD_ID: Deno.env.get("GUILD_ID")!,
   MY_CHANNEL_ID: Deno.env.get("MY_CHANNEL_ID")!,
   ENV: Deno.env.get("DENO_ENV")!,
+  AUTH_ENDPOINT: Deno.env.get("AUTH_ENDPOINT")!,
+  DISCORD_CLIENT_ID: Deno.env.get("DISCORD_CLIENT_ID")!,
+  DISCORD_CLIENT_SECRET: Deno.env.get("DISCORD_CLIENT_SECRET")!,
+  CRYPTO_KEY: Deno.env.get("CRYPTO_KEY")!,
 };
