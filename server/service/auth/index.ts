8ff067f8618d8f78ff5a79bc5f4790ac383b1593
miso-devel <kengo279@icloud.com>
2024-01-03 22:39:32 +0900

    encryptとdecryptを追加

--- server/service/auth/index.ts
+++ server/service/auth/index.ts
@@ -1,5 +1,5 @@
 import { SECRET } from "../../constants/secret.ts";
-import { webcrypto } from "../../deps.ts";
+import { Buffer, crypto } from "../../deps.ts";
 
 type TAccessToken = {
   access_token: string;
@@ -8,6 +8,10 @@ type TAccessToken = {
   token_type: string;
   scope: string;
 };
+
+const ALGO = "aes-128-cbc";
+const PASSWORD = SECRET.CRYPTO_PASSWORD;
+const SALT = SECRET.CRYPTO_SALT;
 
 /**
  * トークンを取得して返す
@@ -20,7 +24,7 @@ export const getAccessToken = async (code: string): Promise<TAccessToken> => {
       client_id: SECRET.DISCORD_CLIENT_ID,
       client_secret: SECRET.DISCORD_CLIENT_SECRET,
       grant_type: "authorization_code",
-      code: code,
+      code,
       redirect_uri: SECRET.SERVER_URL + "/auth/token",
       scope: "identify guilds",
     }),
@@ -39,44 +43,41 @@ export const checkToken = async (accessToken: string): Promise<boolean> => {
   return res.user ? true : false;
 };
 
-type TEncryptToken = { iv: Uint8Array; encryptedData: string };
+/**
+ * cookieに保存するときに暗号化するための関数
+ * 暗語化したデータとivをbase64に変換して返している
+ */
+export const encrypt = (data: string): string => {
+  const bufferData = Buffer.from(data);
+  const key = crypto.scryptSync(PASSWORD, SALT, 16);
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(ALGO, key, iv);
 
-// cookieに保存するときに暗号化するための関数
-export const encryptToken = async (token: string): Promise<TEncryptToken> => {
-  const key = await webcrypto.crypto.subtle.importKey(
-    "raw",
-    new TextEncoder().encode(SECRET.CRYPTO_KEY),
-    "AES-GCM",
-    false,
-    ["encrypt"],
+  let encryptedData = cipher.update(bufferData);
+  encryptedData = Buffer.concat([encryptedData, cipher.final()]);
+
+  return btoa(
+    JSON.stringify({
+      iv: iv.toString("base64"),
+      encryptedData: encryptedData.toString("base64"),
+    }),
   );
-
-  const iv = webcrypto.crypto.getRandomValues(new Uint8Array(16));
-  const encrypted = await webcrypto.crypto.subtle.encrypt(
-    { name: "AES-GCM", iv },
-    key,
-    new TextEncoder().encode(token),
-  );
-  const encryptedData = new TextDecoder().decode(encrypted);
-
-  return { iv, encryptedData };
 };
 
-// cookieから取り出して復号化するための関数
-export const decryptToken = async (token: string): Promise<string> => {
-  const key = await webcrypto.crypto.subtle.importKey(
-    "raw",
-    new TextEncoder().encode(SECRET.CRYPTO_KEY),
-    "AES-GCM",
-    false,
-    ["encrypt"],
-  );
+type TEncryptedData = { iv: string; encryptedData: string };
 
-  const decryptedToken = await webcrypto.crypto.subtle.decrypt(
-    { name: "AES-GCM", iv: new TextEncoder().encode(SECRET.CRYPTO_KEY) },
-    key,
-    new TextEncoder().encode(token),
-  );
+/**
+ * cookieから取り出したデータを復号化するための関数
+ * cookieに保存するときに暗号化したデータとivをbase64化しているので、parseしてivと暗号化したデータを取り出している
+ */
+export const decrypt = (data: string): string => {
+  const { iv, encryptedData }: TEncryptedData = JSON.parse(atob(data));
+  const decodedIv = Buffer.from(iv, "base64");
+  const decodedEncryptedData = Buffer.from(encryptedData, "base64");
+  const key = crypto.scryptSync(PASSWORD, SALT, 16);
+  const decipher = crypto.createDecipheriv(ALGO, key, decodedIv);
 
-  return new TextDecoder().decode(decryptedToken);
+  let decryptedData = decipher.update(decodedEncryptedData);
+  decryptedData = Buffer.concat([decryptedData, decipher.final()]);
+  return decryptedData.toString("utf-8");
 };
