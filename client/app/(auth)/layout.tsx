0b002b15f6aa0d220fd8a53cf58b81fcfacaf7c4
miso-devel <kengo279@icloud.com>
2024-01-07 23:33:49 +0900

    nextAuthからユーザー情報をとってこないようにする

--- client/app/(auth)/layout.tsx
+++ client/app/(auth)/layout.tsx
@@ -1,9 +1,11 @@
 import { AppLayout } from '../../components/layout/AppLayout';
-import { authOptions } from '../../lib/auth';
-import { getServerSession } from 'next-auth';
+
 import { ReactNode } from 'react';
 
 export default async function PageLayout({ children }: { children: ReactNode }) {
-  const session = await getServerSession(authOptions);
-  return <main>{session && <AppLayout session={session}>{children}</AppLayout>}</main>;
+  return (
+    <main>
+      <AppLayout>{children}</AppLayout>
+    </main>
+  );
 }
