7b57cf2376ce5a971cf0344ed3e7727bd4b94949
miso-devel <kengo279@icloud.com>
2024-01-08 20:13:07 +0900

    DiscordのAPIから取得してきたときの型を追加する

---
+++ server/service/user/type.ts
@@ -0,0 +1,14 @@
+export type TDiscordUser = {
+  id: string;
+  username: string;
+  avatar: string;
+  discriminator: string;
+  public_flags: number;
+  premium_type: number;
+  flags: number;
+  banner?: number;
+  accent_color: number;
+  global_name: string;
+  avatar_decoration_data?: null;
+  banner_color: string;
+};
