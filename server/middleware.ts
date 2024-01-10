1ce420d1d356f05b9f7b21018e4d55b5e66f1146
miso-devel <kengo279@icloud.com>
2024-01-10 23:54:15 +0900

    middlewareを一旦雑に追加

---
+++ server/middleware.ts
@@ -0,0 +1,19 @@
+import { SECRET } from "./constants/secret.ts";
+import { Context, getCookie, Next } from "./deps.ts";
+import { checkToken, decrypt, parseTokenData } from "./service/auth/index.ts";
+import { throwAPIError } from "./util/throwError.ts";
+
+export const authMiddleware = async (c: Context, next: Next) => {
+  const accessToken = getCookie(c, "accessToken");
+  console.log(accessToken);
+  if (!accessToken) throwAPIError(403, "Not Found");
+  const decryptedAccessToken = decrypt(accessToken as string);
+  const requiredTokenData = parseTokenData(decryptedAccessToken);
+  const isValidToken = await checkToken(requiredTokenData.access_token);
+
+  if (isValidToken) {
+    await next();
+  } else {
+    return c.redirect(SECRET.CLIENT_URL as string);
+  }
+};
