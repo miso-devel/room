4d2978798be76971bb44288b42191d14e8efb246
miso-devel <kengo279@icloud.com>
2024-01-13 02:19:44 +0900

    signin時にトークンを求めないようにした

--- server/main.ts
+++ server/main.ts
@@ -1,33 +1,14 @@
-import { SECRET } from "./constants/secret.ts";
-import { cors, Hono, logger, poweredBy } from "./deps.ts";
+import { Hono } from "./deps.ts";
 import Auth from "./controller/auth/index.ts";
 import Workshop from "./controller/workshops/index.ts";
 import User from "./controller/users/index.ts";
 import Event from "./controller/events/index.ts";
-import { authMiddleware } from "./middleware.ts";
+import { middlewareOptions } from "./middleware.ts";
 
 const app = new Hono();
 
-app.use(
-  "*",
-  cors({
-    origin: [SECRET.CLIENT_URL as string],
-    allowHeaders: [
-      "Origin",
-      "Content-Type",
-      "Authorization",
-      "X-Custom-Header",
-      "Access-Control-Allow-Origin",
-    ],
-    allowMethods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-    exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
-    maxAge: 600,
-  }),
-  logger(),
-  poweredBy(),
-  authMiddleware,
-);
+app.use("*", ...middlewareOptions({ auth: true }));
+app.use("/auth/signin", ...middlewareOptions({ auth: false }));
 
 app.route("/auth", Auth);
 app.route("/users", User);
--- server/middleware.ts
+++ server/middleware.ts
@@ -2,6 +2,8 @@ import { SECRET } from "./constants/secret.ts";
 import { Context, getCookie, Next } from "./deps.ts";
 import { checkToken, decrypt, parseTokenData } from "./service/auth/index.ts";
 import { throwAPIError } from "./util/throwError.ts";
+import { cors, logger, poweredBy } from "./deps.ts";
+import { MiddlewareHandler } from "https://deno.land/x/hono@v3.10.3/types.ts";
 
 export const authMiddleware = async (c: Context, next: Next) => {
   const accessToken = getCookie(c, "accessToken");
@@ -16,3 +18,29 @@ export const authMiddleware = async (c: Context, next: Next) => {
     return c.redirect(SECRET.CLIENT_URL as string);
   }
 };
+
+export const middlewareBaseOptions = [
+  cors({
+    origin: [SECRET.CLIENT_URL as string],
+    allowHeaders: [
+      "Origin",
+      "Content-Type",
+      "Authorization",
+      "X-Custom-Header",
+      "Access-Control-Allow-Origin",
+    ],
+    allowMethods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+    exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
+    maxAge: 600,
+  }),
+  logger(),
+  poweredBy(),
+];
+
+export const middlewareOptions = (
+  { auth }: { auth: boolean },
+): MiddlewareHandler[] => [
+  ...middlewareBaseOptions,
+  ...(auth ? [authMiddleware] : []),
+];
