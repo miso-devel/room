01b7fcd46a519f8b0bf54dac83bb0510c6a6102e
miso-devel <kengo279@icloud.com>
2024-01-18 22:15:42 +0900

    headers使えなくなったのでcookiesに置き換える

--- client/util/fetcher/index.ts
+++ client/util/fetcher/index.ts
@@ -1,29 +1,35 @@
-import { headers } from 'next/headers';
+import { cookies } from 'next/headers';
 
 const get = async <T>(url: string, init?: RequestInit): Promise<T> => {
+  const withCookieHeader = { headers: { Cookie: cookies().get('accessToken')?.value ?? 'aaaaa' } };
+  console.log('withCookieHeader', withCookieHeader);
   const baseUrl = typeof window ? process.env.NEXT_PUBLIC_SERVER_URL : process.env.SERVER_URL;
-  const res = await fetch(baseUrl + url, { ...headers(), ...init }).then((res) => res.json());
+  const res = await fetch(baseUrl + url, { ...withCookieHeader, ...init }).then((res) => res.json());
   return res;
 };
 
 const post = async <I, T>(url: string, data: I, init?: RequestInit): Promise<T> => {
+  const withCookieHeader = { headers: { Cookie: cookies().get('accessToken')?.value ?? '' } };
   const baseUrl = typeof window ? process.env.NEXT_PUBLIC_SERVER_URL : process.env.SERVER_URL;
   const res = await fetch(baseUrl + url, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json', ...headers() },
+    headers: { 'Content-Type': 'application/json', ...withCookieHeader.headers },
     body: JSON.stringify(data),
     mode: 'cors',
     credentials: 'include',
     ...init,
-  }).then((res) => res.json());
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
   return res;
 };
 
 const patch = async <I, T>(url: string, data: I, init?: RequestInit): Promise<T> => {
+  const withCookieHeader = { headers: { Cookie: cookies().get('accessToken')?.value ?? '' } };
   const baseUrl = typeof window ? process.env.NEXT_PUBLIC_SERVER_URL : process.env.SERVER_URL;
   const res = await fetch(baseUrl + url, {
     method: 'PATCH',
-    headers: { 'Content-Type': 'application/json', ...headers() },
+    headers: { 'Content-Type': 'application/json', ...withCookieHeader.headers },
     body: JSON.stringify(data),
     mode: 'cors',
     credentials: 'include',
@@ -33,10 +39,11 @@ const patch = async <I, T>(url: string, data: I, init?: RequestInit): Promise<T>
 };
 
 const _delete = async <I, T>(url: string, data: I, init?: RequestInit): Promise<T> => {
+  const withCookieHeader = { headers: { Cookie: cookies().get('accessToken')?.value ?? '' } };
   const baseUrl = typeof window ? process.env.NEXT_PUBLIC_SERVER_URL : process.env.SERVER_URL;
   const res = await fetch(baseUrl + url, {
     method: 'DELETE',
-    headers: { 'Content-Type': 'application/json', ...headers() },
+    headers: { 'Content-Type': 'application/json', ...withCookieHeader.headers },
     body: JSON.stringify(data),
     mode: 'cors',
     credentials: 'include',
