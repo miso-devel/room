f61f47060bfc965176b0d9b23c38b740a3c933de
miso-devel <kengo279@icloud.com>
2024-01-18 22:22:55 +0900

    ユーザー取得ドメイン変更対応

--- client/app/(auth)/events/[workshopId]/new/page.tsx
+++ client/app/(auth)/events/[workshopId]/new/page.tsx
@@ -5,7 +5,7 @@ import { fetcher } from '../../../../../util/fetcher';
 
 export default async function NewEvent({ params }: { params: { workshopId: string } }) {
   const workshop = await fetcher.get<schema['Workshop']>(`/workshops/${params.workshopId}`);
-  const members = await fetcher.get<schema['User'][]>('/members');
+  const members = await fetcher.get<schema['User'][]>('/users');
 
   return (
     <>
