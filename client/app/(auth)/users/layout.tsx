3ea744d333c0106b4a46913a82891202cd826a00
miso-devel <kengo279@icloud.com>
2024-01-20 18:00:40 +0900

    memberをuserに統一する

--- client/app/(auth)/users/layout.tsx
+++ client/app/(auth)/users/layout.tsx
@@ -3,8 +3,8 @@ import { WithTitleWrapper } from '../../../components/layout/WithTitleWrapper';
 
 export default async function PageLayout({ children }: { children: ReactNode }) {
   return (
-    <WithTitleWrapper title="メンバー一覧">
-      <p className="mb-5">一旦メンバー一覧とかは取れなくてもいい気がするので実装はしていない</p>
+    <WithTitleWrapper title="ユーザー一覧">
+      <p className="mb-5">一旦ユーザー一覧とかは取れなくてもいい気がするので実装はしていない</p>
       {children}
     </WithTitleWrapper>
   );
--- client/constants/paths.ts
+++ client/constants/paths.ts
@@ -1,6 +1,6 @@
 export const PATHS = [
   { name: '/home', text: 'HOME' },
-  { name: '/members', text: 'MEMBER' },
-  { name: '/workshops', text: 'WORKSHOP' },
-  { name: '/events', text: 'EVENT' },
+  { name: '/users', text: 'USERS' },
+  { name: '/workshops', text: 'WORKSHOPS' },
+  { name: '/events', text: 'EVENTS' },
 ];
