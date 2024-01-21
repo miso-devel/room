0aff92676e58415be130dbad344cf4365d869a8f
miso-devel <kengo279@icloud.com>
2024-01-21 23:34:17 +0900

    使ってないコンポーネントを削除

--- client/components/layout/UserDropdownMenu.tsx
+++
@@ -1,9 +0,0 @@
-import { Session } from 'next-auth';
-import Image from 'next/image';
-import { FC } from 'react';
-
-type TUserDropdownMenu = { session: Session };
-
-export const UserDropdownMenu: FC<TUserDropdownMenu> = ({ session }) => {
-  return <Image src={session?.user?.image as string} alt="me" width={50} height={50} className=" rounded-full" />;
-};
