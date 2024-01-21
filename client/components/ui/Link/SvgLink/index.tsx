2a271a6b00cacbe8dad8ad546bbdc33104b2ffc2
miso-devel <kengo279@icloud.com>
2024-01-21 21:51:11 +0900

    svgImageを使った汎用的なLinkコンポーネントも作成

--- client/components/ui/Link/SvgLink/index.tsx
+++ client/components/ui/Link/SvgLink/index.tsx
@@ -1,12 +1,14 @@
 import { FC, ReactNode } from 'react';
 import NextLink from 'next/link';
+import { SvgImage } from '../../Image/SvgImage';
 
-type TSvgLinkProps = { href: string; children: ReactNode; ariaLabel: string };
+type TSvgLinkProps = { href: string; children?: ReactNode; svgName: string; svgAlt: string; ariaLabel: string };
 
-export const SvgLink: FC<TSvgLinkProps> = ({ href, children, ariaLabel }) => {
+export const SvgLink: FC<TSvgLinkProps> = ({ href, children, svgName, svgAlt, ariaLabel }) => {
   return (
     <NextLink href={href} aria-label={ariaLabel}>
       {children}
+      <SvgImage svgName={svgName} svgAlt={svgAlt} ariaLabel={ariaLabel} />
     </NextLink>
   );
 };
