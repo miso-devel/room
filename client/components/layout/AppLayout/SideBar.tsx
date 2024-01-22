0298a9868d35763d50695d2a103116331912babc
miso-devel <kengo279@icloud.com>
2024-01-22 21:57:45 +0900

    sidebar固定

--- client/components/layout/AppLayout/SideBar.tsx
+++ client/components/layout/AppLayout/SideBar.tsx
@@ -5,12 +5,12 @@ import { Me } from './Me';
 
 export const SideBar: FC = () => {
   return (
-    <div className="flex h-screen w-[15%] flex-col gap-1 rounded-r-3xl bg-secondary px-5 text-bright">
+    <aside className="fixed flex h-screen w-[15%] flex-col gap-1 rounded-r-3xl bg-secondary px-5 text-bright">
       <Link href="/" className="my-10 ms-2 text-2xl font-extrabold hover:opacity-70">
         Doer
       </Link>
       <Navigations />
       <Me />
-    </div>
+    </aside>
   );
 };
