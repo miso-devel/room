5e8fb6ec334f039b6a7c5abacb02a7c8c4fd7335
miso-devel <kengo279@icloud.com>
2024-01-27 18:47:41 +0900

    nextのmiddlewareで見る必要がない気がした

--- client/middleware.ts
+++ client/middleware.ts
@@ -3,17 +3,17 @@ import type { NextRequest } from 'next/server';
 import { fetcher } from './util/fetcher';
 
 export async function middleware(request: NextRequest) {
-  try {
-    const isValid = await fetcher.get<{ hasValidToken: boolean }>('/auth/token/check', {
-      credentials: 'include',
-      cache: 'no-cache',
-      headers: request.headers,
-    });
-    if (!isValid.hasValidToken) return NextResponse.redirect(new URL('/', request.url));
-  } catch (error) {
-    console.log('error', error);
-    return NextResponse.redirect(new URL('/', request.url));
-  }
+  // try {
+  //   const isValid = await fetcher.get<{ hasValidToken: boolean }>('/auth/token/check', {
+  //     credentials: 'include',
+  //     cache: 'no-cache',
+  //     headers: request.headers,
+  //   });
+  //   if (!isValid.hasValidToken) return NextResponse.redirect(new URL('/', request.url));
+  // } catch (error) {
+  //   console.log('error', error);
+  //   return NextResponse.redirect(new URL('/', request.url));
+  // }
 }
 
 export const config = { matcher: ['/home', '/members', '/workshop', '/events'] };
