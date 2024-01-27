d04d72df2566962c6b7a6c53f5caf563f2041814
miso-devel <kengo279@icloud.com>
2024-01-28 05:02:37 +0900

    spinnerを背景白でも見えるように

--- client/components/ui/Spinner/index.tsx
+++ client/components/ui/Spinner/index.tsx
@@ -1,9 +1,14 @@
 import { FC } from 'react';
 
-export const Spinner: FC = () => {
+type TSpinner = FC<{ dark?: boolean }>;
+export const Spinner: TSpinner = ({ dark }) => {
   return (
     <div className="m-auto flex justify-center" aria-label="読み込み中">
-      <div className="h-5 w-5 animate-spin rounded-full border-2 border-white border-t-transparent" />
+      <div
+        className={`h-5 w-5 animate-spin rounded-full border-2 ${
+          dark ? 'border-primary' : 'border-white'
+        }  border-t-transparent`}
+      />
     </div>
   );
 };
