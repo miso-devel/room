d6d230e8ec38537886f0a07e9afbb54880bca823
miso-devel <kengo279@icloud.com>
2024-01-31 01:00:41 +0900

    suspenseのためにcomponentを分ける

--- client/app/(auth)/workshops/[workshopId]/edit/page.tsx
+++ client/app/(auth)/workshops/[workshopId]/edit/page.tsx
@@ -1,11 +1,18 @@
-import { Suspense } from 'react';
+import { FC, Suspense } from 'react';
 import { WorkshopEditForm } from './_components/workshopEditForm';
 import { Spinner } from '../../../../../components/ui/Spinner';
+import { fetcher } from '../../../../../util/fetcher';
+import { schema } from '../../../../../types/common';
 
-export default async function WorkshopsShowPage({ params }: { params: { workshopId: string } }) {
+export const WorkshopShowPageWrap: FC<{ workshopId: string }> = async ({ workshopId }) => {
+  const workshop = await fetcher.get<schema['Workshop']>(`/workshops/${workshopId}`);
+  return <WorkshopEditForm workshop={workshop} />;
+};
+
+export default function WorkshopsShowPage({ params }: { params: { workshopId: string } }) {
   return (
     <Suspense fallback={<Spinner dark />}>
-      <WorkshopEditForm workshopId={params.workshopId} />
+      <WorkshopShowPageWrap workshopId={params.workshopId} />
     </Suspense>
   );
 }
