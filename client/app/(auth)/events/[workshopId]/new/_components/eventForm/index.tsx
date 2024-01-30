f3ce7cb2bfb0bcb61648d9fc28ccbe0c8cf98382
miso-devel <kengo279@icloud.com>
2024-01-31 01:18:28 +0900

    actionとbutton分ける

--- client/app/(auth)/events/[workshopId]/new/_components/eventForm/index.tsx
+++ client/app/(auth)/events/[workshopId]/new/_components/eventForm/index.tsx
@@ -1,41 +1,24 @@
+'use client';
 import { FC } from 'react';
-import { redirect } from 'next/navigation';
 import { schema } from '../../../../../../../types/common';
 import { FormWrapper } from '../../../../../../../components/ui/Form/FormWrapper';
 import { Input } from '../../../../../../../components/ui/Form/Input';
-import { Button } from '../../../../../../../components/ui/Button';
 import { UserSelectModal } from '../UserSelectModal';
-import { fetcher } from '../../../../../../../util/fetcher';
 import { Workshop } from '../workshop';
+import { submit } from './actions';
+import { EventNewButton } from '../eventNewButton';
 
-type TEventForm = FC<{ workshopId: string }>;
+type TEventForm = FC<{ workshop: schema['Workshop']; users: schema['User'][] }>;
 
-export const EventForm: TEventForm = async ({ workshopId }) => {
-  const workshop = await fetcher.get<schema['Workshop']>(`/workshops/${workshopId}`);
-  const members = await fetcher.get<schema['User'][]>('/users');
-
-  const submit = async (formData: FormData) => {
-    'use server';
-    const theme = formData.get('theme') as string;
-    const datetime = formData.get('datetime') as string;
-    const discordIds = formData.getAll('discordIds') as string[];
-    const eventInput: schema['EventInput'] = {
-      event: { workshopId, theme, datetime },
-      discordIds: discordIds,
-    };
-
-    const event = await fetcher.post<schema['EventInput'], schema['Event']>('/events', eventInput);
-    redirect(`/workshops/${event.workshopId}`);
-  };
-
+export const EventForm: TEventForm = async ({ workshop, users }) => {
   return (
     <>
       <Workshop workshop={workshop} />
-      <FormWrapper action={submit}>
+      <FormWrapper action={async (form) => await submit(form, workshop.id)}>
         <Input label="テーマ" name="theme" placeholder="theme here" type="text" required />
         <Input label="時間" name="datetime" placeholder="date here" type="datetime-local" required />
-        <UserSelectModal users={members} />
-        <Button>作成する</Button>
+        <UserSelectModal users={users} />
+        <EventNewButton />
       </FormWrapper>
     </>
   );
