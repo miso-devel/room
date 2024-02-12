3bae519025283ae30ab4f350896442b6bac7c3af
miso-devel <kengo279@icloud.com>
2024-02-12 20:50:17 +0900

    middlewareでもdecryptできない時の処理を書いておく

--- server/middleware.ts
+++ server/middleware.ts
@@ -7,8 +7,13 @@ import { cors, logger, poweredBy } from "./deps.ts";
 export const authMiddleware = async (c: Context, next: Next) => {
   // middlewareを通すときにaccessToken=xxxxxという形で渡ってこないのでない時はheaderからとってきている
   const accessToken = getCookie(c, "accessToken") ?? c.req.header()["cookie"];
-  if (!accessToken) throwAPIError(403, "Not Found");
+  if (!accessToken) throwAPIError(401, "Not Found")();
   const decryptedAccessToken = decrypt(accessToken as string);
+  if (!decryptedAccessToken) {
+    // 適当な文字列を入れられるとdecryptに失敗するので失敗した時のエラー処理も書いておく
+    return throwAPIError(401, "accessToken is invalid")();
+  }
+
   const requiredTokenData = parseTokenData(decryptedAccessToken);
   const isValidToken = await checkToken(requiredTokenData.access_token);
 
