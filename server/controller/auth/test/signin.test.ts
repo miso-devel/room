644a7d7d471afe9ef0041bcdfb419a6606591115
miso-devel <kengo279@icloud.com>
2024-02-12 23:08:14 +0900

    とりあえずsigninのテストを追加する

---
+++ server/controller/auth/test/signin.test.ts
@@ -0,0 +1,16 @@
+import { SECRET } from "../../../constants/secret.ts";
+import { assertEquals } from "../../../deps.ts";
+import { app } from "../../../main.ts";
+import { TEST } from "../../../testUtil/constants.ts";
+
+Deno.test("/signin", async (t) => {
+  const res = await app.request(TEST.URL + "/auth/signin");
+  await t.step("200で返ってくる", () => assertEquals(res.status, 200));
+  await t.step(
+    "適切なレスポンスが返ってくる",
+    async () => {
+      const data = await res.json();
+      assertEquals(data.redirectUrl, SECRET.AUTH_ENDPOINT);
+    },
+  );
+});
