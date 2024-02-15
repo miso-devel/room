5bed856dde1235e3482a88459973acf1ea1df8f6
miso-devel <kengo279@icloud.com>
2024-02-16 01:09:12 +0900

    cookie関連のテストを追加する

--- server/controller/auth/test/token.test.ts
+++ server/controller/auth/test/token.test.ts
@@ -1,33 +1,31 @@
-import { returnsNext } from "https://deno.land/std@0.215.0/testing/mock.ts";
-import { SECRET } from "../../../constants/secret.ts";
-import { assertEquals, assertSpyCallArgs, stub } from "../../../deps.ts";
+import { assertEquals, stub } from "../../../deps.ts";
 import { app } from "../../../main.ts";
-import * as authService from "../../../service/auth/index.ts";
 import { TAccessToken } from "../../../service/auth/type.ts";
 import { TEST } from "../../../testUtil/constants.ts";
 
 Deno.test("/token", async (t) => {
-  const requests = {
-    noCode: await app.request(TEST.URL + "/auth/token"),
-    inValidCode: await app.request(TEST.URL + "/auth/token?code=123"),
-  };
-
   await t.step(
     "異常系",
     async (t) => {
       await t.step(
         "そもそもcodeがない場合",
-        () => assertEquals(requests.noCode.status, 401),
+        async () => {
+          const res = await app.request(TEST.URL + "/auth/token");
+          assertEquals(res.status, 401);
+        },
       );
 
       await t.step(
         "codeが正常なものでない場合",
-        () => assertEquals(requests.inValidCode.status, 401),
+        async () => {
+          const res = await app.request(TEST.URL + "/auth/token?code=123");
+          assertEquals(res.status, 401);
+        },
       );
     },
   );
 
-  await t.step("正常系", async () => {
+  await t.step("正常系", async (t) => {
     const accessTokenMock: TAccessToken = {
       access_token: "access_token",
       refresh_token: "refresh_token",
@@ -35,22 +33,62 @@ Deno.test("/token", async (t) => {
       token_type: "token_type",
       scope: "scope",
     };
-    const mockFetchText = JSON.stringify(accessTokenMock);
+    const mockFetchRes = JSON.stringify(accessTokenMock);
+    // 処理中でdiscordのOAuth2.0の認可エンドポイントにアクセスしてるのだが、テストではアクセスしたくないため、fetchをstubしている
     const fetchStub = stub(
       globalThis,
       "fetch",
       () =>
         Promise.resolve(
-          new Response(mockFetchText, {
+          new Response(mockFetchRes, {
             headers: { "Content-Type": "application/json" },
           }),
         ),
     );
-    const data = await app.request(TEST.URL + "/auth/token?code=123456");
+    const res = await app.request(TEST.URL + "/auth/token?code=123456");
+
     try {
-      t.step(
+      await t.step(
         "適切なレスポンスが返ってくる",
-        () => assertEquals(data.status, 302),
+        () => assertEquals(res.status, 302),
+      );
+
+      await t.step(
+        "意図したcookieがセットされている",
+        async (t) => {
+          const [accessToken, maxAge, path, httpOnly, secure, sameSite] = res
+            .headers.get("set-cookie")?.split(";")
+            .map((f) => f.trim()) as string[];
+          await t.step(
+            "accessTokenがある",
+            () => assertEquals(accessToken?.split("=")[0], "accessToken"),
+          );
+          await t.step(
+            "maxAgeがある",
+            () => assertEquals(maxAge?.split("="), ["Max-Age", "100"]),
+          );
+          await t.step(
+            "pathがある",
+            () => assertEquals(path?.split("="), ["Path", "/"]),
+          );
+          await t.step(
+            "httpOnlyがある",
+            () => assertEquals(httpOnly, "HttpOnly"),
+          );
+          await t.step(
+            "secureがある",
+            () => assertEquals(secure, "Secure"),
+          );
+          await t.step(
+            "sameSiteがある",
+            () =>
+              assertEquals(
+                sameSite?.split("="),
+                ["SameSite", "Lax"],
+                "sameSiteがLaxになっている",
+              ),
+          );
+        },
       );
     } finally {
       fetchStub.restore();
