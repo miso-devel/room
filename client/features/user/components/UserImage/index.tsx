281d0bd86cab1288c71d10ba7cf3b8432b99a052
miso-devel <kengo279@icloud.com>
2024-02-18 15:36:57 +0900

    format feature

--- client/features/user/components/UserImage/index.tsx
+++ client/features/user/components/UserImage/index.tsx
@@ -1,20 +1,18 @@
-import { FC } from 'react';
-import Image from 'next/image';
+import Image from 'next/image'
+import type { FC } from 'react'
 
-type TUserImageProps = { name: string; avatarPath: string };
+type UserImageProps = { name: string; avatarPath: string }
 
-export const UserImage: FC<TUserImageProps> = ({ name, avatarPath }) => {
-  const splited = avatarPath?.split('/');
+export const UserImage: FC<UserImageProps> = ({ name, avatarPath }) => {
+  const splited = avatarPath?.split('/')
 
   return (
     <>
       {splited[splited.length - 1] !== 'NaN.png' ? (
-        <Image src={avatarPath} alt={`${name}のアバター画像`} width={25} height={25} className="rounded-full" />
+        <Image src={avatarPath} alt={`${name}のアバター画像`} width={25} height={25} className='rounded-full' />
       ) : (
-        <>
-          <div className="h-[25px] w-[25px] rounded-full bg-secondary" />
-        </>
+        <div className='h-[25px] w-[25px] rounded-full bg-secondary' />
       )}
     </>
-  );
-};
+  )
+}
--- client/features/user/components/UserProfile/index.tsx
+++ client/features/user/components/UserProfile/index.tsx
@@ -1,13 +1,12 @@
-import { FC } from 'react';
-import { UserImage } from '../UserImage';
+import type { FC } from 'react'
+import { UserImage } from '../UserImage'
 
-type TUserProfileProps = { name: string; avatarPath: string };
-
-export const UserProfile: FC<TUserProfileProps> = ({ name, avatarPath }) => {
+type UserProfileProps = { name: string; avatarPath: string }
+export const UserProfile: FC<UserProfileProps> = ({ name, avatarPath }) => {
   return (
-    <div className="flex items-center gap-2">
+    <div className='flex items-center gap-2'>
       <UserImage name={name} avatarPath={avatarPath} />
-      <p className="text-sm">{name}</p>
+      <p className='text-sm'>{name}</p>
     </div>
-  );
-};
+  )
+}
