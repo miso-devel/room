c9ce0e3aa3f708bc0e636fbe5204581ade1116c9
miso-devel <kengo279@icloud.com>
2024-02-18 16:28:44 +0900

    format index

--- client/app/(index)/components/LoginButton.tsx
+++ client/app/(index)/components/LoginButton.tsx
@@ -1,23 +1,23 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import { Button } from '../../../components/ui/Button';
+'use client'
+import { useRouter } from 'next/navigation'
+import { Button } from '../../../components/ui/Button'
 
 export const LoginButton = async () => {
-  const router = useRouter();
+  const router = useRouter()
   const signIn = async () => {
-    const res: { redirectUrl: string } = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + '/auth/signin', {
+    const res: { redirectUrl: string } = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/auth/signin`, {
       method: 'GET',
       headers: { 'Content-Type': 'application/json' },
       mode: 'cors',
       credentials: 'include',
     })
-      .then((res) => res.json())
-      .catch((err) => console.error(err));
-    if (res.redirectUrl) router.push(res.redirectUrl);
-  };
+      .then(res => res.json())
+      .catch(err => console.error(err))
+    if (res.redirectUrl) router.push(res.redirectUrl)
+  }
   return (
     <Button onClick={signIn} onDark>
       Login with Discord
     </Button>
-  );
-};
+  )
+}
--- client/app/(index)/page.tsx
+++ client/app/(index)/page.tsx
@@ -1,10 +1,10 @@
-import { LoginButton } from './components/LoginButton';
+import { LoginButton } from './components/LoginButton'
 
 export default function App() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center bg-secondary text-bright">
-      <h1 className="mb-5 text-3xl">Doer</h1>
+    <main className='flex min-h-screen w-full flex-col items-center justify-center bg-secondary text-bright'>
+      <h1 className='mb-5 text-3xl'>Doer</h1>
       <LoginButton />
     </main>
-  );
+  )
 }
