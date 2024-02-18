26192a98fe0b31af9ffbb56aefdde766de8732fa
miso-devel <kengo279@icloud.com>
2024-02-18 22:16:41 +0900

    error handle追加

--- client/util/error/index.tsx
+++ client/util/error/index.tsx
@@ -1,9 +1,12 @@
-import { redirect } from 'next/navigation';
+import { redirect } from 'next/navigation'
 
 export const errorHandler = (res: Response) => {
   switch (res.status) {
     case 401:
-      console.log('401', 'Auth Error');
-      redirect('/');
+      console.log('401', 'Auth Error')
+      redirect('/')
+      break
+    default:
+      throw new Error(`API error: ${res.statusText}`)
   }
-};
+}
