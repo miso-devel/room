e379ad0f864519a501859b77d0b3536dae89f8fd
miso-devel <kengo279@icloud.com>
2024-02-19 21:32:31 +0900

    userをswrでとってくる

--- client/components/layout/AppLayout/Me.tsx
+++ client/components/layout/AppLayout/Me.tsx
@@ -1,11 +1,25 @@
+'use client'
+import { Spinner } from '@/components/ui/Spinner'
 import { UserProfile } from '@/features/user/components/UserProfile'
 import type { Schema } from '@/types/common'
-import { fetcher } from '@/util/fetcher'
 import type { FC, ReactNode } from 'react'
+import useSwr from 'swr'
 import { LogoutButton } from '../SignOutButton'
 
-export const Me: FC = async () => {
-  const user: Schema['User'] = await fetcher.get('/users/me')
+const fetcher = async (key: string) => {
+  return await fetch(key, { mode: 'cors', credentials: 'include' })
+    .then(res => res.json())
+    .catch(_ => {
+      throw new Error('Failed to fetch user data: user')
+    })
+}
+
+export const Me: FC = () => {
+  const { data: user, error, isLoading } = useSwr<Schema['User']>(`${process.env.NEXT_PUBLIC_SERVER_URL}/users/me`, fetcher)
+
+  if (error) return <p>取得に失敗しました</p>
+  if (isLoading || !(user?.name && user?.avatar)) return <Spinner />
+
   return (
     <>
       <UserProfile name={user.name} avatarPath={user.avatar} />
