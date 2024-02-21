880c9b0fb345330620390ac9d9504964bee060c1
miso-devel <kengo279@icloud.com>
2024-02-21 20:26:58 +0900

    typeの追加

--- client/components/ui/Button/index.tsx
+++ client/components/ui/Button/index.tsx
@@ -9,7 +9,7 @@ type ButtonProps = ComponentProps<'button'> & {
 export const Button: FC<ButtonProps> = ({ children, onDark, sm, ...rest }) => {
   return (
     <button
-      type='button'
+      type={rest.type || 'button'}
       className={`${onDark ? 'border-bright hover:border-transparent hover:text-dark' : 'hover:border-dark'} ${
         sm ? 'border p-1 text-sm' : 'border-2 p-2 text-base'
       } rounded-md transition-all hover:bg-accent disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:bg-transparent`}
