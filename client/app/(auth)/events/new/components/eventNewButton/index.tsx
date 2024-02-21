99a3c1d93c6334d9f04e9e89c3c4e8dbf882197f
miso-devel <kengo279@icloud.com>
2024-02-21 21:46:02 +0900

    eventをsubmitできるように

--- client/app/(auth)/events/new/components/eventNewButton/index.tsx
+++ client/app/(auth)/events/new/components/eventNewButton/index.tsx
@@ -6,5 +6,9 @@ import { useFormStatus } from 'react-dom'
 export const EventNewButton = () => {
   const { pending } = useFormStatus()
 
-  return <Button disabled={pending}>{pending ? <Spinner dark /> : '作成'}</Button>
+  return (
+    <Button type='submit' disabled={pending}>
+      {pending ? <Spinner dark /> : '作成'}
+    </Button>
+  )
 }
