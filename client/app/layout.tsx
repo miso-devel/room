0895d5ff225f3aa2ded3ab62ed8ca175f0a152a9
miso-devel <kengo279@icloud.com>
2024-02-24 00:35:25 +0900

    layout変更

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import type { ReactNode } from 'react'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -6,7 +7,7 @@ export const metadata: Metadata = {
   description: 'Doerの管理サイト',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='ja'>
       <body className='font-sans font-semibold'>{children}</body>
