824d305a6fc7cd3e4349b906944d05122d4119be
miso-devel <kengo279@icloud.com>
2024-02-28 22:47:50 +0900

    buttonのimport変える

--- client/app/(index)/components/LoginButton.tsx
+++ client/app/(index)/components/LoginButton.tsx
@@ -1,6 +1,6 @@
 'use client'
+import { Button } from '@/components/ui/Button'
 import { useRouter } from 'next/navigation'
-import { Button } from '../../../components/ui/Button'
 
 export const LoginButton = async () => {
   const router = useRouter()
