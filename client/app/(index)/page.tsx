0270ac3ee872f17b4e0fd357fd83da4a54c60261
miso-devel <kengo279@icloud.com>
2024-03-01 00:43:50 +0900

    勉強会とは限らない

--- client/app/(index)/page.tsx
+++ client/app/(index)/page.tsx
@@ -3,8 +3,7 @@ import { LoginButton } from './components/LoginButton'
 export default function App() {
   return (
     <main className='flex min-h-screen w-full flex-col items-center justify-center bg-secondary text-bright'>
-      <h1 className='text-3xl'>Do'er</h1>
-      <p className='mt-2 mb-5 font-normal'>Do'erの勉強会サーバーです</p>
+      <h1 className='text-3xl mb-5'>Do'er</h1>
       <LoginButton />
     </main>
   )
